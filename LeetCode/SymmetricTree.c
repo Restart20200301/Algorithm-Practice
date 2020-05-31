f414995f81d87dc00c0aca04e498f173fb071033
WantToLearnWeb <354678962@qq.com>
2020-05-31 20:43:34 +0800

    symmetric tree

---
+++ LeetCode/SymmetricTree.c
@@ -0,0 +1,46 @@
+/**
+给定一个二叉树，检查它是否是镜像对称的。 
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool isMirror(struct TreeNode* left, struct TreeNode* right) {
+    if (left && right) 
+        return left->val == right->val && isMirror(left->left, right->right) && isMirror(left->right, right->left);
+    return left == right;
+}
+
+bool isSymmetric(struct TreeNode* root){
+    if (root == NULL)
+        return true;
+    return isMirror(root->left, root->right);
+}
