ededdb5cf719a24419ee26e06fc19f4cba4cdedb
WantToLearnWeb <354678962@qq.com>
2020-09-29 21:48:13 +0800

    new

---
+++ LeetCode/_0145_BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,47 @@
+/*
+给定一个二叉树，返回它的 后序 遍历。
+
+示例:
+
+输入: [1,null,2,3]  
+   1
+    \
+     2
+    /
+   3 
+
+输出: [3,2,1]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-postorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorderTraversal(root, ans);
+        return ans;
+    }
+
+    void postorderTraversal(TreeNode* p, vector<int>& ans) {
+        if (!p) return;
+        postorderTraversal(p->left, ans);
+        postorderTraversal(p->right, ans);
+        ans.push_back(p->val);
+    }
+};
